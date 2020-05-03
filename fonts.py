6ad7b87234e4c4e0f3462393dbf966768e7ca3f7
Joanna Muras <joanna.muras@onet.eu>
2020-05-03 17:28:46 +0200

    Fix generation of text with spaces

--- fonts.py
+++ fonts.py
@@ -9,18 +9,16 @@ class Fonts:
 
 
     def getText(self, text, resolution, colour):
-        text_width, text_height = self.font.getsize(text)
         width, height = resolution
-        image = Image.new('RGB', (text_width, text_height), (0, 0, 0))
-        draw = ImageDraw.Draw(image)
-        draw.text((0, 0), text, colour, self.font)
-        image = image.crop((0, text_height - height, text_width, text_height))
 
         pixelsArray = []
-        for i in range(0, text_width, width + 1):
-            box = (i, 0, i + width, height)
-            letter = image.crop(box)
-            letterPixels = Pixels(letter)
+        for sign in text:
+            text_width, text_height = self.font.getsize(sign)
+            image = Image.new('RGB', (text_width, text_height), (0, 0, 0))
+            draw = ImageDraw.Draw(image)
+            draw.text((0, 0), sign, colour, self.font)
+            image = image.crop((0, text_height - height, width, text_height))
+            letterPixels = Pixels(image)
             pixelArray = letterPixels.convert(resolution)
             pixelsArray.append(pixelArray)
 
